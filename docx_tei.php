b0f0f4a6b3b97d6b32c492e3bac178941c2b8384
Frédéric Glorieux <frederic.glorieux@fictif.org>
2024-02-12 14:25:07 +0100

    zotero move

---
+++ docx_tei.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+include_once(__DIR__ . '/vendor/autoload.php');
+
+use Psr\Log\LogLevel;
+use Oeuvres\Kit\{Cliglob, Filesys, Log};
+use Oeuvres\Kit\Logger\{LoggerCli};
+use Oeuvres\Teinte\Format\{Docx};
+
+
+
+class Rougemont extends Cliglob
+{
+    /** Object to load Tei for exports */
+    static $docx;
+    const SRC_FORMAT = "docx";
+    const SRC_EXT = ".docx";
+    const DST_FORMAT = "tei";
+    const DST_EXT = ".xml";
+
+    public static function cli()
+    {
+        self::$docx = new Docx();
+        Log::setLogger(new LoggerCli(LogLevel::DEBUG));
+        // local xml template ?
+        // self::$docx->user_template(__DIR__ . '/piaget_tmpl.xml');
+        self::glob([__CLASS__, 'export']);
+    }
+
+
+    static function export($src_file, $dst_file)
+    {
+        Log::info($src_file . " > " . $dst_file);
+        self::$docx->open($src_file);
+        // self::$docx->teiURI($dst_file);
+        // debug
+        self::$docx->pkg();
+        self::$docx->teilike();
+        file_put_contents($dst_file .'_teilike.xml', self::$docx->teiXML());
+        self::$docx->pcre(); // apply regex, custom re may break XML
+        file_put_contents($dst_file .'_pcre.xml', self::$docx->teiXML());
+        self::$docx->tmpl();
+        file_put_contents($dst_file, self::$docx->teiXML());
+    }
+}
+
+if (Rougemont::isCli()) {
+    Rougemont::cli();
+}
